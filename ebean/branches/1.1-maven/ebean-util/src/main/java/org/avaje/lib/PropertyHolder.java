6e69beee7c14611cfcf265bee2110f27f89e2466
pmendelson <pmendelson@391a5f12-5583-4198-a4bc-3c26488f1c65>
2009-01-28 03:20:21 +0000

    Created ebean-util to hold helper methods that various ebean subprojects use.

---
+++ ebean/branches/1.1-maven/ebean-util/src/main/java/org/avaje/lib/PropertyHolder.java
@@ -0,0 +1,33 @@
+/**
+ *  Copyright (C) 2006  Robin Bygrave
+ *  
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package org.avaje.lib;
+
+/**
+ * Used by PropertyEvaluator to evaluate properties.
+ * <p>
+ * Holds property values that can contain expressions such as ${CATALINA_HOME}
+ * or ${JNDI:...} that use environment variables or jndi settings.
+ * </p>
+ */
+public interface PropertyHolder {
+
+	/**
+	 * Return the value of the named property.
+	 */
+	public String getProperty(String name);
+}
