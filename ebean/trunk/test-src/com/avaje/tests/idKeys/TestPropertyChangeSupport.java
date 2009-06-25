238d3ee401f771f871453cfee22225e5143d8be4
imario <imario@391a5f12-5583-4198-a4bc-3c26488f1c65>
2009-06-25 10:53:37 +0000

    use enhancement for tests, added PropertyChangeSupport tests

---
+++ ebean/trunk/test-src/com/avaje/tests/idKeys/TestPropertyChangeSupport.java
@@ -0,0 +1,126 @@
+package com.avaje.tests.idKeys;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.EbeanServer;
+import com.avaje.ebean.Transaction;
+import com.avaje.ebean.bean.EntityBean;
+import com.avaje.tests.idKeys.db.AuditLog;
+import junit.framework.TestCase;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Test various aspects of the PropertyChangeSupport
+ */
+public class TestPropertyChangeSupport extends TestCase implements PropertyChangeListener
+{
+    private int nuofEvents = 0;
+    private PropertyChangeEvent lastPce;
+
+    public void propertyChange(PropertyChangeEvent evt)
+    {
+        nuofEvents++;
+        lastPce = evt;
+    }
+
+    /**
+     * Test the core property change functionality 
+     */
+    public void testPropertyChange() throws SQLException
+    {
+        resetEvent();
+
+        AuditLog al = new AuditLog();
+
+        assertTrue("is EntityBean check", al instanceof EntityBean);
+
+        addListener(al, this);
+        
+        // test if we get property notification about simple property changes
+        al.setDescription("ABC");
+
+        assertNotNull(lastPce);
+        assertEquals(1, nuofEvents);
+        assertEquals("description", lastPce.getPropertyName());
+        assertNull(lastPce.getOldValue());
+        assertEquals("ABC", lastPce.getNewValue());
+
+        al.setDescription("DEF");
+
+        assertNotNull(lastPce);
+        assertEquals(2, nuofEvents);
+        assertEquals("description", lastPce.getPropertyName());
+        assertEquals("ABC", lastPce.getOldValue());
+        assertEquals("DEF", lastPce.getNewValue());
+
+        resetEvent();
+
+        // test if we get change notification if EBean assigns an id to the bean
+        EbeanServer server = Ebean.getServer(null);
+        server.save(al);
+
+        assertNotNull(lastPce);
+        assertEquals("id", lastPce.getPropertyName());
+        assertNull(lastPce.getOldValue());
+        assertNotNull(lastPce.getNewValue());
+
+        String prevLogDesc = al.getDescription();
+
+        resetEvent();
+
+        // simulate external change and test if we get change notification when we refresh the entity
+        Transaction tx = server.beginTransaction();
+        PreparedStatement pstm = tx.getConnection().prepareStatement("update audit_log set description = ? where id = ?");
+        pstm.setString(1, "GHI");
+        pstm.setLong(2, al.getId());
+        int updated = pstm.executeUpdate();
+        pstm.close();
+
+        assertEquals(1, updated);
+
+        tx.commit();
+
+        assertNull(lastPce);
+        assertEquals(0, nuofEvents);
+
+        server.refresh(al);
+
+        assertNotNull(lastPce);
+        assertEquals(1, nuofEvents);
+        assertEquals("description", lastPce.getPropertyName());
+        assertEquals(prevLogDesc, lastPce.getOldValue());
+        assertNotNull("GHI", lastPce.getNewValue());
+    }
+
+    private void resetEvent()
+    {
+        lastPce = null;
+        nuofEvents = 0;
+    }
+
+    private static void addListener(AuditLog al, PropertyChangeListener listener)
+    {
+        try
+        {
+            Method apcs = al.getClass().getMethod("addPropertyChangeListener", PropertyChangeListener.class);
+            apcs.invoke(al, listener);
+        }
+        catch (NoSuchMethodException e)
+        {
+            throw new RuntimeException(e);
+        }
+        catch (IllegalAccessException e)
+        {
+            throw new RuntimeException(e);
+        }
+        catch (InvocationTargetException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ ebean/trunk/test-src/com/avaje/tests/idKeys/db/AuditLog.java
@@ -0,0 +1,38 @@
+package com.avaje.tests.idKeys.db;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(name = "AD_SEQ_NAME", sequenceName = "AD_SEQ")
+public class AuditLog
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "AD_SEQ_NAME")
+    private Long id;
+
+    private String description;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+}
