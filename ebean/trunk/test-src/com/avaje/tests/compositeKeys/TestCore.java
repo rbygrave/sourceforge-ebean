9095e02bf03ca959b696fc48966d2a88823d7982
imario <imario@391a5f12-5583-4198-a4bc-3c26488f1c65>
2009-06-25 09:47:28 +0000

    ebean tests - first regarding composite-key handling. TestCore acutally fails due to EBean bug :-)

---
+++ ebean/trunk/test-src/com/avaje/tests/compositeKeys/TestCore.java
@@ -0,0 +1,105 @@
+package com.avaje.tests.compositeKeys;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.EbeanServer;
+import com.avaje.ebean.Query;
+import com.avaje.ebean.Transaction;
+import com.avaje.ebean.expression.Expr;
+import com.avaje.tests.compositeKeys.db.Item;
+import com.avaje.tests.compositeKeys.db.ItemKey;
+import com.avaje.tests.compositeKeys.db.Region;
+import com.avaje.tests.compositeKeys.db.RegionKey;
+import com.avaje.tests.compositeKeys.db.Type;
+import com.avaje.tests.compositeKeys.db.TypeKey;
+import junit.framework.TestCase;
+
+import java.util.List;
+
+/**
+ * Test some of the Avaje core functionality in conjunction with composite keys like
+ * <ul>
+ * <li>write</li>
+ * <li>find</li>
+ * </ul>
+ */
+public class TestCore extends TestCase
+{
+    @Override
+    protected void setUp() throws Exception
+    {
+        EbeanServer server = Ebean.getServer(null);
+
+        Transaction tx = server.beginTransaction();
+
+        Type type = new Type();
+        TypeKey typeKey = new TypeKey();
+        typeKey.setCustomer(1);
+        typeKey.setType(10);
+        type.setKey(typeKey);
+        type.setDescription("Type Old-Item - Customer 1");
+        server.save(type);
+
+        type = new Type();
+        typeKey = new TypeKey();
+        typeKey.setCustomer(2);
+        typeKey.setType(10);
+        type.setKey(typeKey);
+        type.setDescription("Type Old-Item - Customer 2");
+        server.save(type);
+
+        Region region = new Region();
+        RegionKey regionKey = new RegionKey();
+        regionKey.setCustomer(1);
+        regionKey.setType(500);
+        region.setKey(regionKey);
+        region.setDescription("Region West - Customer 1");
+        server.save(region);
+
+        region = new Region();
+        regionKey = new RegionKey();
+        regionKey.setCustomer(2);
+        regionKey.setType(500);
+        region.setKey(regionKey);
+        region.setDescription("Region West - Customer 2");
+        server.save(region);
+
+        Item item = new Item();
+        ItemKey itemKey = new ItemKey();
+        itemKey.setCustomer(1);
+        itemKey.setItemNumber("ITEM1");
+        item.setKey(itemKey);
+        item.setDescription("Fancy Car - Customer 1");
+        item.setRegion(500);
+        item.setType(10);
+        server.save(item);
+
+        item = new Item();
+        itemKey = new ItemKey();
+        itemKey.setCustomer(2);
+        itemKey.setItemNumber("ITEM1");
+        item.setKey(itemKey);
+        item.setDescription("Another Fancy Car - Customer 2");
+        item.setRegion(500);
+        item.setType(10);
+        server.save(item);
+
+        tx.commit();
+    }
+
+    public void testFind()
+    {
+        EbeanServer server = Ebean.getServer(null);
+
+        List<Item> items = server.find(Item.class).findList();
+
+        assertNotNull(items);
+        assertEquals(2, items.size());
+
+        Query<Item> qItems = server.find(Item.class);
+        qItems.where(Expr.eq("key.customer", Integer.valueOf(1)));
+        items = qItems.findList();
+
+        assertNotNull(items);
+        assertEquals(1, items.size());
+    }
+}
