92c85d4482ce501e58435bef32ac82ecbf13b3d1
Rob Bygrave <208973+rbygrave@users.noreply.github.com>
2009-07-09 13:48:43 +0000

    Fix for enhancer plugin (so not dependent on external packages)

--- ebean/trunk/src/com/avaje/ebean/enhance/agent/EnhancedTransactional.java
+++ ebean/trunk/src/com/avaje/ebean/enhance/agent/EnhancedTransactional.java
@@ -17,7 +17,7 @@
  * along with Ebean; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA  
  */
-package com.avaje.ebean.common;
+package com.avaje.ebean.enhance.agent;
 
 /**
  * Marker interface for classes enhanced to support Transactional methods.
