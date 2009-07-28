302e7f4e97febccdca50f08b065c87e7d4a5a6bc
Rob Bygrave <208973+rbygrave@users.noreply.github.com>
2009-07-28 10:33:23 +0000

    Test objects

--- ebean/branches/2.0-maven/core/src/test/java/com/avaje/tests/lib/EbeanTestCase.java
+++ ebean/branches/2.0-maven/core/src/test/java/com/avaje/tests/lib/EbeanTestCase.java
@@ -1,41 +1,41 @@
 package com.avaje.tests.lib;
 
-import com.avaje.ebean.Ebean;
-import com.avaje.ebean.EbeanServer;
-import com.avaje.ebean.Transaction;
 import junit.framework.TestCase;
 import junit.framework.TestResult;
 
-/**
- * The base class for all Ebean test to get access to the Ebean server and do some cleanup stuff after a test has run
- */
-public class EbeanTestCase extends TestCase
-{
-    @Override
-    public void run(TestResult testResult)
-    {
-        try
-        {
-            super.run(testResult);
-        }
-        finally
-        {
-            Transaction tx = getServer().currentTransaction();
-            if (tx != null && tx.isActive())
-            {
-                // transaction left running after the test, rollback it to make the environment ready for the next test
-                tx.rollback();
-            }
-        }
-    }
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.EbeanServer;
+import com.avaje.ebean.Transaction;
 
-    public EbeanServer getServer()
-    {
-    	try {
-        return Ebean.getServer(null);
-    	} catch (Exception e){
-    		e.printStackTrace();
-    		throw new RuntimeException(e);
-    	}
-    }
+/**
+ * The base class for all Ebean test to get access to the Ebean server and do
+ * some cleanup stuff after a test has run
+ */
+public class EbeanTestCase extends TestCase {
+	
+	@Override
+	public void run(TestResult testResult) {
+		try {
+			super.run(testResult);
+		} finally {
+			Transaction tx = getServer().currentTransaction();
+			if (tx != null && tx.isActive()) {
+				// transaction left running after the test, rollback it to make
+				// the environment ready for the next test
+				tx.rollback();
+			}
+		}
+	}
+	
+//	/**
+//	 * Dummy test method to keep surefire happy.
+//	 */
+//	public void testDummy(){
+//		Assert.assertTrue(true);
+//	}
+
+	public EbeanServer getServer() {
+		
+		return Ebean.getServer(null);
+	}
 }
