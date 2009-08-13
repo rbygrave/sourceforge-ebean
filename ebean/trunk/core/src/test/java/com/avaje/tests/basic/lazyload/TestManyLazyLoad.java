9ad9e7269efa507ff18db32e51587ce832ab3276
rob107 <rob107@391a5f12-5583-4198-a4bc-3c26488f1c65>
2009-08-13 04:08:41 +0000

    Update tests (only reset data once to keep ids)

--- ebean/trunk/core/src/test/java/com/avaje/tests/basic/lazyload/TestManyLazyLoad.java
+++ ebean/trunk/core/src/test/java/com/avaje/tests/basic/lazyload/TestManyLazyLoad.java
@@ -1,5 +1,6 @@
 package com.avaje.tests.basic.lazyload;
 
+import java.sql.Date;
 import java.util.List;
 
 import junit.framework.Assert;
@@ -26,12 +27,33 @@ public class TestManyLazyLoad extends TestCase {
 		// get it as a reference
 		Order order1 = Ebean.getReference(Order.class, order.getId());
 		Assert.assertNotNull(order1);
+		
+		Date orderDate = order1.getOrderDate();
+		Assert.assertNotNull(orderDate);
+		
 		List<OrderDetail> details = order1.getDetails();
 		
 		// lazy load the details
 		int sz = details.size();
 		Assert.assertTrue("sz > 0", sz > 0);
 		
+		Order o = details.get(0).getOrder();
+		Assert.assertTrue("same instance", o == order1);
+		
+
+		// change order... list before a scalar property
+		Order order2 = Ebean.getReference(Order.class, order.getId());
+		Assert.assertNotNull(order2);
+		
+		List<OrderDetail> details2 = order2.getDetails();
+		
+		// lazy load the details
+		int sz2 = details2.size();
+		Assert.assertTrue("sz2 > 0", sz2 > 0);
+		
+		Order o2 = details2.get(0).getOrder();
+		Assert.assertTrue("same instance", o2 == order2);
+
 		
 	}
 	
--- ebean/trunk/core/src/test/java/com/avaje/tests/basic/lazyload/TestTransient.java
+++ ebean/trunk/core/src/test/java/com/avaje/tests/basic/lazyload/TestTransient.java
@@ -3,6 +3,7 @@ package com.avaje.tests.basic.lazyload;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import com.avaje.ebean.BeanState;
 import com.avaje.ebean.Ebean;
 import com.avaje.tests.model.basic.Customer;
 import com.avaje.tests.model.basic.ResetBasicData;
@@ -20,6 +21,10 @@ public class TestTransient extends TestCase {
 			.findUnique();
 		
 		Assert.assertNotNull(c);
+
+		BeanState beanState = Ebean.getBeanState(c);
+		Assert.assertFalse("not new or dirty as transient", beanState.isNewOrDirty());
+
 		
 		c.getLock().tryLock();
 		try {
@@ -32,15 +37,31 @@ public class TestTransient extends TestCase {
 		Boolean selected = c.getSelected();
 		Assert.assertNotNull(selected);
 		
+		Assert.assertFalse("not new or dirty as transient", beanState.isNewOrDirty());
+		
 		Ebean.save(c);
 		
 		selected = c.getSelected();
 		Assert.assertNotNull(selected);
 		
 		c.setName("Modified");
+		Assert.assertTrue("dirty now", beanState.isNewOrDirty());
+
 		
 		selected = c.getSelected();
 		Assert.assertNotNull(selected);
 		
+		Ebean.save(c);
+		Assert.assertFalse("Not dirty after save", beanState.isNewOrDirty());
+
+		selected = c.getSelected();
+		Assert.assertNotNull(selected);
+		
+		String updateStmt = "update customer set name = 'Rob' where id = :id";
+		int rows = Ebean.createUpdate(Customer.class, updateStmt)
+			.set("id", 1)
+			.execute();
+		
+		Assert.assertTrue("changed name back", 1 == rows);
 	}
 }
