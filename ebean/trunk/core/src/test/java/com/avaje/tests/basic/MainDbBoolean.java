6de7c95d0910506c9dff539ff4864c8ac4734c15
Rob Bygrave <208973+rbygrave@users.noreply.github.com>
2009-08-28 23:05:24 +0000

    Update test with more asserts

--- ebean/trunk/core/src/test/java/com/avaje/tests/basic/MainDbBoolean.java
+++ ebean/trunk/core/src/test/java/com/avaje/tests/basic/MainDbBoolean.java
@@ -7,8 +7,10 @@ import junit.framework.Assert;
 import com.avaje.ebean.EbeanServer;
 import com.avaje.ebean.EbeanServerFactory;
 import com.avaje.ebean.Query;
+import com.avaje.ebean.SqlRow;
 import com.avaje.ebean.config.DataSourceConfig;
 import com.avaje.ebean.config.ServerConfig;
+import com.avaje.ebean.config.dbplatform.Postgres83Platform;
 import com.avaje.tests.model.basic.TOne;
 
 public class MainDbBoolean {
@@ -33,13 +35,13 @@ public class MainDbBoolean {
 		postgresDb.setUrl("jdbc:postgresql://127.0.0.1:5432/test");
 		postgresDb.setHeartbeatSql("select count(*) from t_one");
 
-		// requires oracle driver in class path
-		DataSourceConfig oraDb = new DataSourceConfig();
-		oraDb.setDriver("oracle.jdbc.driver.OracleDriver");
-		oraDb.setUsername("junk");
-		oraDb.setPassword("junk");
-		oraDb.setUrl("jdbc:oracle:thin:junk/junk@localhost:1521:XE");
-		oraDb.setHeartbeatSql("select count(*) from dual");
+//		// requires oracle driver in class path
+//		DataSourceConfig oraDb = new DataSourceConfig();
+//		oraDb.setDriver("oracle.jdbc.driver.OracleDriver");
+//		oraDb.setUsername("junk");
+//		oraDb.setPassword("junk");
+//		oraDb.setUrl("jdbc:oracle:thin:junk/junk@localhost:1521:XE");
+//		oraDb.setHeartbeatSql("select count(*) from dual");
 		
 		
 		c.loadFromProperties();
@@ -52,10 +54,10 @@ public class MainDbBoolean {
 		
 		//c.setDatabaseBooleanTrue("1");
 		//c.setDatabaseBooleanFalse("0");
-		//c.setDatabaseBooleanTrue("T");
-		//c.setDatabaseBooleanFalse("F");
+		c.setDatabaseBooleanTrue("T");
+		c.setDatabaseBooleanFalse("F");
 
-		//c.setDatabasePlatform(new Postgres83Platform());
+		c.setDatabasePlatform(new Postgres83Platform());
 
 		c.addClass(TOne.class);
 
@@ -85,7 +87,20 @@ public class MainDbBoolean {
 			.orderBy("id")
 			.findList();
 		
-		System.out.println(list);
+		Assert.assertTrue(list.size() == 2);
+		Assert.assertTrue(list.get(0).isActive());
+		Assert.assertFalse(!list.get(0).isActive());
+		
+		String sql = "select id, name, active from t_oneb order by id";
+		List<SqlRow> sqlRows = server.createSqlQuery(sql).findList();
+		Assert.assertTrue(sqlRows.size() == 2);
+		Object active0 = sqlRows.get(0).get("active");
+		Object active1 = sqlRows.get(1).get("active");
+		
+		Assert.assertTrue("T".equals(active0));
+		Assert.assertTrue("F".equals(active1));
+		
+		
 
 		Query<TOne> query = server.find(TOne.class)
 			.setAutofetch(false)
