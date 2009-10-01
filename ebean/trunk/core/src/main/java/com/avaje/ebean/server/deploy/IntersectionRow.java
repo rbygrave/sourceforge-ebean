9b9e1df2f4eb6666f8f10fac9d9803d10dacbe24
nedge <nedge@391a5f12-5583-4198-a4bc-3c26488f1c65>
2009-10-01 17:43:04 +0000

    fix for bug in SQL for delete of ManyToMany

--- ebean/trunk/core/src/main/java/com/avaje/ebean/server/deploy/IntersectionRow.java
+++ ebean/trunk/core/src/main/java/com/avaje/ebean/server/deploy/IntersectionRow.java
@@ -13,25 +13,25 @@ import com.avaje.ebean.server.core.DefaultSqlUpdate;
 public class IntersectionRow {
 
 	String tableName;
-	
+
 	Map<String,Object> values = new LinkedHashMap<String,Object>();
-	
+
 	public IntersectionRow(String tableName){
 		this.tableName = tableName;
 	}
-	
+
 	public void put(String key, Object value){
 		values.put(key, value);
 	}
-	
+
 	public SqlUpdate createInsert(EbeanServer server){
 
 
 		BindParams bindParams = new BindParams();
-		
+
 		StringBuilder sb = new StringBuilder();
 		sb.append("insert into ").append(tableName).append(" (");
-		
+
 		int count = 0;
 		Iterator<Entry<String, Object>> it = values.entrySet().iterator();
 		while (it.hasNext()) {
@@ -41,10 +41,10 @@ public class IntersectionRow {
 
 			Map.Entry<String, Object> entry = it.next();
 			sb.append(entry.getKey());
-			
+
 			bindParams.setParameter(count, entry.getValue());
 		}
-		
+
 		sb.append(") values (");
 		for (int i = 0; i < count; i++) {
 			if (i > 0){
@@ -53,7 +53,7 @@ public class IntersectionRow {
 			sb.append("?");
 		}
 		sb.append(")");
-		
+
 		return new DefaultSqlUpdate(server, sb.toString(), bindParams);
 	}
 
@@ -61,26 +61,27 @@ public class IntersectionRow {
 
 
 		BindParams bindParams = new BindParams();
-		
+
 
 		StringBuilder sb = new StringBuilder();
 		sb.append("delete from ").append(tableName).append(" where ");
-		
+
 		int count = 0;
 		Iterator<Entry<String, Object>> it = values.entrySet().iterator();
 		while (it.hasNext()) {
 			if (count++ > 0){
-				sb.append(", ");
+				sb.append(" and ");
 			}
+
 			Map.Entry<String, Object> entry = it.next();
-			
+
 			sb.append(entry.getKey());
 			sb.append(" = ?");
-			
+
 			bindParams.setParameter(count, entry.getValue());
 		}
-		
-		return new DefaultSqlUpdate(server, sb.toString(), bindParams);		
+
+		return new DefaultSqlUpdate(server, sb.toString(), bindParams);
 	}
 
 }
