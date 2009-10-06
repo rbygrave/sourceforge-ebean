74c9ad398da8e1860559f1e140b6093ceef16896
imario <imario@391a5f12-5583-4198-a4bc-3c26488f1c65>
2009-10-06 11:58:41 +0000

    BUG 164: wrong sql generated when target entity uses inheritance

--- ebean/trunk/core/src/test/java/com/avaje/tests/inheritance/TestInheritInsert.java
+++ ebean/trunk/core/src/test/java/com/avaje/tests/inheritance/TestInheritInsert.java
@@ -1,10 +1,13 @@
 package com.avaje.tests.inheritance;
 
 import com.avaje.ebean.Ebean;
+import com.avaje.ebean.Query;
+import com.avaje.tests.model.basic.Car;
 import com.avaje.tests.model.basic.VehicleDriver;
-
 import junit.framework.Assert;
 import junit.framework.TestCase;
+
+import java.util.List;
 
 public class TestInheritInsert extends TestCase {
 
@@ -20,5 +23,27 @@ public class TestInheritInsert extends TestCase {
 		Assert.assertNotNull(driver);
 		
 	}
-	
+
+	public void testQuery()
+	{
+		Car car = new Car();
+		car.setLicenseNumber("MARIOS_CAR_LICENSE");
+		Ebean.save(car);
+
+		VehicleDriver driver = new VehicleDriver();
+		driver.setName("Mario");
+		driver.setVehicle(car);
+		Ebean.save(driver);
+
+		Query<VehicleDriver> query = Ebean.find(VehicleDriver.class);
+		query.where().eq("vehicle.licenseNumber", "MARIOS_CAR_LICENSE");
+		List<VehicleDriver> drivers = query.findList();
+		
+		Assert.assertNotNull(drivers);
+		Assert.assertEquals(1, drivers.size());
+		Assert.assertNotNull(drivers.get(0));
+
+		Assert.assertEquals("Mario", drivers.get(0).getName());
+		Assert.assertEquals("MARIOS_CAR_LICENSE", drivers.get(0).getVehicle().getLicenseNumber());
+	}
 }
