6099fe986c3219966a217b54062ea211bb2ba6df
Rob Bygrave <208973+rbygrave@users.noreply.github.com>
2009-12-02 07:18:58 +0000

    Update test for ManyToMany

--- ebean/trunk/core/src/test/java/com/avaje/tests/basic/TestM2MVanilla.java
+++ ebean/trunk/core/src/test/java/com/avaje/tests/basic/TestM2MVanilla.java
@@ -12,38 +12,49 @@ import com.avaje.tests.model.basic.MUser;
 
 public class TestM2MVanilla extends TestCase {
 
-	public void testVanilla() {
+    public void testVanilla() {
 
-		MRole r1 = new MRole();
-		r1.setRoleName("role1");
-		Ebean.save(r1);
-		
-		MRole r2 = new MRole();
-		r2.setRoleName("role2");
-		Ebean.save(r2);
-		
-		
-		MUser u0 = new MUser();
-		u0.setUserName("something");
-		
-		Ebean.save(u0);
-		
-		MUser user = Ebean.find(MUser.class, u0.getUserid());
-		
-		List<MRole> roleList = new ArrayList<MRole>();
-		roleList.add(r1);
-		roleList.add(r2);
-		
-		user.setRoles(roleList);
-		
-		Ebean.save(user);
-		
-		MUser checkUser = Ebean.find(MUser.class, u0.getUserid());
-		List<MRole> checkRoles = checkUser.getRoles();
-		Assert.assertNotNull(checkRoles);
-		Assert.assertTrue(checkRoles.size() == 2);
-		
-		
-	}
-	
+        MRole r1 = new MRole();
+        r1.setRoleName("role1");
+        Ebean.save(r1);
+
+        MRole r2 = new MRole();
+        r2.setRoleName("role2");
+        Ebean.save(r2);
+
+        MRole r3 = new MRole();
+        r3.setRoleName("role3");
+        Ebean.save(r3);
+
+        MUser u0 = new MUser();
+        u0.setUserName("something");
+
+        Ebean.save(u0);
+
+        MUser user = Ebean.find(MUser.class, u0.getUserid());
+
+        List<MRole> roleList = new ArrayList<MRole>();
+        roleList.add(r1);
+        roleList.add(r2);
+
+        user.setRoles(roleList);
+
+        Ebean.save(user);
+
+        MUser checkUser = Ebean.find(MUser.class, u0.getUserid());
+        List<MRole> checkRoles = checkUser.getRoles();
+        Assert.assertNotNull(checkRoles);
+        Assert.assertTrue(checkRoles.size() == 2);
+
+        checkRoles.add(r3);
+        
+        Ebean.save(checkUser);
+        
+        MUser checkUser2 = Ebean.find(MUser.class, u0.getUserid());
+        List<MRole> checkRoles2 = checkUser2.getRoles();
+        Assert.assertNotNull(checkRoles2);
+        Assert.assertEquals("added a role", 3, checkRoles2.size());
+        
+    }
+
 }
