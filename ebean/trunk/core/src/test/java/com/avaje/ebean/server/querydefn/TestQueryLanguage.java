8429ee2ac456c993be0535e6f031d62e922d01cb
Rob Bygrave <208973+rbygrave@users.noreply.github.com>
2009-12-02 07:21:33 +0000

    Add test for Bug 183 - query language parsing

---
+++ ebean/trunk/core/src/test/java/com/avaje/ebean/server/querydefn/TestQueryLanguage.java
@@ -0,0 +1,93 @@
+package com.avaje.ebean.server.querydefn;
+
+import java.util.Set;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.EbeanServer;
+import com.avaje.tests.model.basic.Order;
+
+
+public class TestQueryLanguage extends TestCase {
+
+    public void test() {
+        
+        
+        DefaultOrmQuery<Order> q = check("find order join customer (id, name)");
+        OrmQueryDetail detail = q.getDetail();
+        OrmQueryProperties chunk = detail.getChunk("customer", false);
+        Set<String> props = chunk.getAllIncludedProperties();
+        
+        Assert.assertTrue(props.contains("id"));
+        Assert.assertTrue(props.contains("name"));
+        
+        q = check("find order join customer(id, name)");
+        detail = q.getDetail();
+        chunk = detail.getChunk("customer", false);
+        props = chunk.getAllIncludedProperties();
+        
+        Assert.assertTrue(props.contains("id"));
+        Assert.assertTrue(props.contains("name"));
+        Assert.assertFalse(chunk.isCache());
+        Assert.assertFalse(chunk.isReadOnly());
+
+        q = check("find order join customer(+cache +readonly, id, name)");
+        detail = q.getDetail();
+        chunk = detail.getChunk("customer", false);
+        props = chunk.getAllIncludedProperties();
+        Assert.assertTrue(props.contains("id"));
+        Assert.assertTrue(props.contains("name"));
+        Assert.assertTrue(chunk.isCache());
+        Assert.assertTrue(chunk.isReadOnly());
+
+        q = check("find order join customer(+cache +readonly,id,name)");
+        detail = q.getDetail();
+        chunk = detail.getChunk("customer", false);
+        props = chunk.getAllIncludedProperties();
+        Assert.assertTrue(props.contains("id"));
+        Assert.assertTrue(props.contains("name"));
+        Assert.assertTrue(chunk.isCache());
+        Assert.assertTrue(chunk.isReadOnly());
+
+        q = check("find order(id,status) join customer(+cache +readonly,id,name)");
+        detail = q.getDetail();
+        chunk = detail.getChunk("customer", false);
+        props = chunk.getAllIncludedProperties();
+        Assert.assertTrue(props.contains("id"));
+        Assert.assertTrue(props.contains("name"));
+        Assert.assertTrue(chunk.isCache());
+        Assert.assertTrue(chunk.isReadOnly());
+
+        chunk = detail.getChunk(null, false);
+        props = chunk.getAllIncludedProperties();
+        Assert.assertTrue(props.contains("id"));
+        Assert.assertTrue(props.contains("status"));
+        Assert.assertFalse(props.contains("orderDate"));
+        
+        q = check("find order(id,status) join customer(+cache +readonly,id,name) where id > :minId order by status");
+        detail = q.getDetail();
+        chunk = detail.getChunk("customer", false);
+        props = chunk.getAllIncludedProperties();
+        Assert.assertTrue(props.contains("id"));
+        Assert.assertTrue(props.contains("name"));
+        Assert.assertTrue(chunk.isCache());
+        Assert.assertTrue(chunk.isReadOnly());
+        
+        String orderBy = q.getOrderByStringFormat();
+        Assert.assertEquals("status", orderBy);
+    }
+    
+    private DefaultOrmQuery<Order> check(String q) {
+
+        EbeanServer server = Ebean.getServer(null);
+
+        OrmQueryDetailParser p = new OrmQueryDetailParser(q);
+        p.parse();
+        DefaultOrmQuery<Order> qry = new DefaultOrmQuery<Order>(Order.class, server);
+        p.assign(qry);
+
+        return qry;
+    }
+}
