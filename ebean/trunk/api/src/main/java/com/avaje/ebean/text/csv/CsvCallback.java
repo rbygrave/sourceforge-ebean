37cdb88c892378fc154cb3849adb049aabe167f5
Rob Bygrave <208973+rbygrave@users.noreply.github.com>
2009-12-05 09:19:01 +0000

    Update CsvCallback API

--- ebean/trunk/api/src/main/java/com/avaje/ebean/text/csv/CsvCallback.java
+++ ebean/trunk/api/src/main/java/com/avaje/ebean/text/csv/CsvCallback.java
@@ -19,10 +19,14 @@
  */
 package com.avaje.ebean.text.csv;
 
+import com.avaje.ebean.EbeanServer;
+
 /**
- * Provides a callback for more custom control over CSV processing.
+ * Provides callback methods for customisation of CSV processing.
  * <p>
- * Allows the developer to further process the bean
+ * You can provide your own CsvCallback implementation to customise the CSV
+ * processing. It is expected that the DefaultCsvCallback provides a good base
+ * class that you can extend.
  * </p>
  * 
  * @author rbygrave
@@ -30,6 +34,34 @@ package com.avaje.ebean.text.csv;
  * @param <T>
  */
 public interface CsvCallback<T> {
+
+    /**
+     * The processing is about to begin.
+     * <p>
+     * Typically the callback will create a transaction, set batch mode, batch
+     * size etc.
+     * </p>
+     */
+    public void begin(EbeanServer server);
+
+    /**
+     * Read the header row.
+     * <p>
+     * This is only called if {@link CsvReader#setHasHeader(boolean)} has been
+     * set to true.
+     * </p>
+     * 
+     * @param line
+     *            the header line content.
+     */
+    public void readHeader(String[] line);
+
+    /**
+     * Check that the row should be processed - return true to process the row
+     * or false to ignore the row. Gives ability to handle bad data... empty
+     * rows etc and ignore it rather than fail.
+     */
+    public boolean processLine(int row, String[] line);
 
     /**
      * Called for each bean after it has been loaded from the CSV content.
@@ -49,5 +81,28 @@ public interface CsvCallback<T> {
      * @param lineContent
      *            the content that has been used to load the bean
      */
-    public void processBean(int row, T bean, String[] lineContent);
+    public void processBean(int row, String[] line, T bean);
+
+    /**
+     * The processing has ended successfully.
+     * <p>
+     * Typically the callback will commit the transaction.
+     * </p>
+     */
+    public void end(int row);
+
+    /**
+     * The processing has ended due to an error.
+     * <p>
+     * This gives the callback the opportunity to rollback the transaction if
+     * one was created.
+     * </p>
+     * 
+     * @param row
+     *            the row that the error has occured on
+     * @param e
+     *            the error that occured
+     */
+    public void endWithError(int row, Exception e);
+
 }
