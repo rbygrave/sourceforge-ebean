67d00971bd4b7b49c15836b4e2a2bb09b351ff5d
rob107 <rob107@391a5f12-5583-4198-a4bc-3c26488f1c65>
2010-03-04 10:45:00 +0000

    Xml play objects

---
+++ ebean/trunk/core/src/test/java/com/avaje/tests/xml/TestXmlSimpleOutput.java
@@ -0,0 +1,85 @@
+package com.avaje.tests.xml;
+
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import junit.framework.TestCase;
+
+import org.w3c.dom.Document;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.internal.SpiEbeanServer;
+import com.avaje.ebean.server.deploy.BeanDescriptor;
+import com.avaje.ebean.server.el.ElPropertyValue;
+import com.avaje.tests.model.basic.Order;
+import com.avaje.tests.model.basic.OrderDetail;
+import com.avaje.tests.model.basic.ResetBasicData;
+
+public class TestXmlSimpleOutput extends TestCase {
+
+    public void test() throws Exception {
+        
+        ResetBasicData.reset();
+        
+        SpiEbeanServer server = (SpiEbeanServer)Ebean.getServer(null);
+        
+        BeanDescriptor<Order> beanDescriptor = server.getBeanDescriptor(Order.class);
+        
+        ElPropertyValue elId = beanDescriptor.getElGetValue("id");
+        ElPropertyValue elSt = beanDescriptor.getElGetValue("status");
+        ElPropertyValue eldate = beanDescriptor.getElGetValue("orderDate");
+        ElPropertyValue elcre = beanDescriptor.getElGetValue("cretime");
+        ElPropertyValue elDetails = beanDescriptor.getElGetValue("details");
+
+        XoAttribute xst = new XoPropAttribute("status", elSt, null);
+        XoNode xid = new XoPropNode("id", elId, null, xst);
+        XoNode xdt = new XoPropNode("ship-date", eldate, null);
+        XoNode xcr = new XoPropNode("created-ts", elcre, null);
+
+
+        BeanDescriptor<OrderDetail> detailDescriptor = server.getBeanDescriptor(OrderDetail.class);
+
+        XoNode detailId = new XoPropNode("id",detailDescriptor.getElGetValue("id"), null);
+        XoNode detailProdName = new XoPropNode("product-name",detailDescriptor.getElGetValue("product.name"), null);
+        
+        XoCompoundNode details = new XoCompoundNode("line", detailId, detailProdName);
+        
+        XoPropCollection xlist = new XoPropCollection("order-details", elDetails, details, true);
+
+        
+        XoCompoundNode orderNode = new XoCompoundNode("order",xid, xcr, xdt, xlist);
+
+        
+        List<Order> list = Ebean.find(Order.class)
+            .join("details")
+            .join("details.product","name")
+            .findList();
+        
+        Order bean = list.get(0);
+        
+        Writer writer = new StringWriter();
+        XmlWriterOutput output = new XmlWriterOutput(writer);
+        
+        orderNode.writeNode(output, bean);
+        
+        System.out.println(writer);
+        
+        DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+        Document doc = docBuilder.newDocument();
+
+        
+        XmlDocumentOutput docOut = new XmlDocumentOutput(doc);
+        
+        
+        orderNode.writeNode(docOut, null, bean);
+        
+        System.out.println(docOut.getDocument().getDocumentElement());
+        
+    }
+    
+}
