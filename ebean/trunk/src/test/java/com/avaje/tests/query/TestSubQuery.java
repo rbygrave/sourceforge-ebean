32b97f1bc091cb539b4b5cfef00332f394207211
Rob Bygrave <208973+rbygrave@users.noreply.github.com>
2010-03-17 06:09:05 +0000

    Update test for regression bug 247

--- ebean/trunk/src/test/java/com/avaje/tests/query/TestSubQuery.java
+++ ebean/trunk/src/test/java/com/avaje/tests/query/TestSubQuery.java
@@ -1,13 +1,15 @@
 package com.avaje.tests.query;
 
-import com.avaje.ebean.Ebean;
-import com.avaje.ebean.Query;
-import com.avaje.tests.model.basic.Order;
-import com.avaje.tests.model.basic.ResetBasicData;
-import junit.framework.TestCase;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import junit.framework.TestCase;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.Query;
+import com.avaje.tests.model.basic.CKeyParent;
+import com.avaje.tests.model.basic.Order;
+import com.avaje.tests.model.basic.ResetBasicData;
 
 public class TestSubQuery extends TestCase {
 
@@ -43,7 +45,7 @@ public class TestSubQuery extends TestCase {
 
     }
 
-	/*
+	
 	public void testCompositeKey()
 	{
 		ResetBasicData.reset();
@@ -62,12 +64,14 @@ public class TestSubQuery extends TestCase {
 		
 		String sql = pq.getGeneratedSql();
 
-		String golden = "(c.one_key) in (select c.one_key c0 from ckey_parent c)";
-
+		//String golden = "(c.one_key) in (select c.one_key c0 from ckey_parent c)";
+		String golden = "(c.one_key) in (select c.one_key  from ckey_parent c) ";
+		
 		if (sql.indexOf(golden) < 0)
 		{
+		    System.out.println("failed sql:"+sql);
 			fail("golden string not found");
 		}
 	}
-	*/
+	
 }
