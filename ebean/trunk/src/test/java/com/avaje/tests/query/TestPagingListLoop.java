95e72f85aca73183656cda1d35df7c59a972e0f3
Rob Bygrave <208973+rbygrave@users.noreply.github.com>
2010-03-17 21:47:52 +0000

    Fix for BUG 248 : When make Paging query the connection is not closed

--- ebean/trunk/src/test/java/com/avaje/tests/query/TestPagingListLoop.java
+++ ebean/trunk/src/test/java/com/avaje/tests/query/TestPagingListLoop.java
@@ -1,46 +1,46 @@
 package com.avaje.tests.query;
 
+import java.util.concurrent.ExecutionException;
+
 import junit.framework.TestCase;
 
 import com.avaje.ebean.Ebean;
+import com.avaje.ebean.PagingList;
+import com.avaje.tests.model.basic.Customer;
 import com.avaje.tests.model.basic.ResetBasicData;
 
 public class TestPagingListLoop extends TestCase {
 
-    public void test() {
+    public void test() throws InterruptedException, ExecutionException {
   
-        boolean autoRunTest = false;
+//        boolean autoRunTest = false;
+//        if (!autoRunTest){
+//            // we only want to run this test manually.
+//            return;
+//        }
         
-        if (!autoRunTest){
-            // we only want to run this test manually.
-            return;
-        }
-        
-//        try {
+        try {
             ResetBasicData.reset();
             
             for (int i = 0; i < 50; i++) {
-                //Ebean.find(Customer.class).findPagingList(10);
-                createLeak();
+                PagingList<Customer> pagingList = Ebean.find(Customer.class).findPagingList(10);
+                pagingList.getFutureRowCount();//.get();
+       
+                //createLeak();
+                Thread.sleep(10);
             }
                         
-//        } catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        
-//        System.out.println("getting busy connections ... ");
-//        
-//        // anything open now is a leak
-//        DataSourceGlobalManager.getDataSource("h2").closeBusyConnections(0);
-        
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+     
     }
     
-    private void createLeak() {
-        
-        // create a transaction we never close ...
-        // ... a connection pool leak
-        Ebean.getServer(null).createTransaction();
-        
-    }
+//    private void createLeak() {
+//        
+//        // create a transaction we never close ...
+//        // ... a connection pool leak
+//        Ebean.getServer(null).createTransaction();
+//    }
     
 }
