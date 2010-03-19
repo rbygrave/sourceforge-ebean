ab60e740d1cd6ea0de93e7b6da5d583691a0ca60
Rob Bygrave <208973+rbygrave@users.noreply.github.com>
2010-03-19 09:24:18 +0000

    Add more tests for automatic query join conversion

--- ebean/trunk/src/test/java/com/avaje/tests/query/TestQueryJoinManyNonRoot.java
+++ ebean/trunk/src/test/java/com/avaje/tests/query/TestQueryJoinManyNonRoot.java
@@ -7,31 +7,79 @@ import junit.framework.TestCase;
 import org.junit.Assert;
 
 import com.avaje.ebean.Ebean;
+import com.avaje.ebean.Query;
 import com.avaje.tests.model.basic.Order;
 import com.avaje.tests.model.basic.ResetBasicData;
 
 public class TestQueryJoinManyNonRoot extends TestCase {
 
-    public void test() {
+    public void test_manyNonRoot() {
         
         ResetBasicData.reset();
         
-        List<Order> list = Ebean.find(Order.class)
+        Query<Order> q = Ebean.find(Order.class)
+            .join("customer")
+            .join("customer.contacts")
+            .where().gt("id", 0)
+            .query();
+
+        List<Order> list = q.findList();
+        String sql = q.getGeneratedSql();
+        
+        Assert.assertTrue(list.size() > 0);
+        Assert.assertTrue(sql.contains("join o_customer oc on oc.id "));
+        Assert.assertTrue(sql.contains("left outer join contact occ on"));
+    
+    }
+
+    public void test_manyRootQueryJoinOrderDetailsFirst() {
+        
+        ResetBasicData.reset();
+        
+        Query<Order> q = Ebean.find(Order.class)
             .join("details")
             .join("details.product")
             .join("customer")
             .join("customer.contacts")
             .where().gt("id", 0)
-            .findList();
+            .query();
 
-        Assert.assertNotNull(list);
-        System.out.println(list);
-      
-        for (Order order : list) {
-            order.getDetails().size();
-            order.getCustomer().getContacts().size();
-        }
-  
+        List<Order> list = q.findList();
+        String sql = q.getGeneratedSql();
+        
+        Assert.assertTrue(list.size() > 0);
+        Assert.assertTrue(sql.contains("join o_customer oc on oc.id "));
+        Assert.assertTrue(sql.contains("left outer join o_order_detail "));
+        Assert.assertTrue(sql.contains("left outer join o_product "));
+                
+        Assert.assertFalse(sql.contains("left outer join contact occ on"));
     
     }
+    
+    
+    public void test_manyRootQueryJoinCustomerContactsFirst() {
+        
+        ResetBasicData.reset();
+        
+        Query<Order> q = Ebean.find(Order.class)
+            .join("customer")
+            .join("customer.contacts")
+            .join("details")
+            .join("details.product")
+            .where().gt("id", 0)
+            .query();
+
+        List<Order> list = q.findList();
+        String sql = q.getGeneratedSql();
+        
+        Assert.assertTrue(list.size() > 0);
+        Assert.assertTrue(sql.contains("join o_customer oc on oc.id "));
+        Assert.assertTrue(sql.contains("left outer join contact occ on"));
+
+        Assert.assertFalse(sql.contains("left outer join o_order_detail "));
+        Assert.assertFalse(sql.contains("left outer join o_product "));
+                
+    
+    }
+
 }
