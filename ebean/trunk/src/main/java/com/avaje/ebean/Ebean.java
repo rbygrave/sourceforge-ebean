a1a1ed1651a519c2f8c963bcb0b49e0a4d6b60c0
rob107 <rob107@391a5f12-5583-4198-a4bc-3c26488f1c65>
2010-03-25 02:07:12 +0000

    v2.5.1

--- ebean/trunk/src/main/java/com/avaje/ebean/Ebean.java
+++ ebean/trunk/src/main/java/com/avaje/ebean/Ebean.java
@@ -139,7 +139,7 @@ public final class Ebean {
     /**
      * The version and date of build.
      */
-    private static final String EBVERSION = "2.5.0";
+    private static final String EBVERSION = "2.5.1";
 
     static {
         String version = System.getProperty("java.version");
