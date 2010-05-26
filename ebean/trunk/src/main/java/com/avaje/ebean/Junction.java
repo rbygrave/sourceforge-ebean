93bcdeb7af176dd6548f1eb44e2bb3206278cdbc
rob107 <rob107@391a5f12-5583-4198-a4bc-3c26488f1c65>
2010-05-26 09:49:27 +0000

    javadoc update

--- ebean/trunk/src/main/java/com/avaje/ebean/Junction.java
+++ ebean/trunk/src/main/java/com/avaje/ebean/Junction.java
@@ -1,5 +1,6 @@
 package com.avaje.ebean;
 
+
 /**
  * Represents a Conjunction or a Disjunction.
  * <p>
@@ -9,12 +10,13 @@ package com.avaje.ebean;
  * <p>
  * Note: where() always takes you to the top level WHERE expression list.
  * </p>
+ * 
  * <pre class="code">
  *  Query q = 
  *    Ebean.find(Person.class)
  *      .where().disjunction()
- *  	  .like(&quot;name&quot;,&quot;Rob%&quot;)
- *  	  .eq(&quot;status&quot;,Status.NEW)
+ *        .like(&quot;name&quot;,&quot;Rob%&quot;)
+ *        .eq(&quot;status&quot;,Status.NEW)
  *  
  *      // where() returns us to the top level expression list
  *     .where().gt(&quot;id&quot;, 10);
@@ -43,7 +45,35 @@ package com.avaje.ebean;
  *     // read as... 
  *     // where ( ((name like Rob%) or (status = NEW)) AND (id > 10) )
  * </pre>
+ * 
+ * <p>
+ * Example of a nested disjunction.
+ * </p>
+ * 
+ * <pre class="code">
+ * Query&lt;Customer&gt; q = 
+ *  Ebean.find(Customer.class)
+ *      .where()
+ *          .disjunction()
+ *              .conjunction()
+ *                  .startsWith(&quot;name&quot;, &quot;r&quot;)
+ *                  .eq(&quot;anniversary&quot;, onAfter)
+ *                  .endJunction()
+ *              .conjunction()
+ *                  .eq(&quot;status&quot;, Customer.Status.ACTIVE)
+ *                  .gt(&quot;id&quot;, 0)
+ *                  .endJunction()
+ *      .order().asc(&quot;name&quot;);
+ * 
+ * q.findList();
+ * String s = q.getGeneratedSql();
+ * 
+ *  // this produces an expression like:
+ *  
+ *  ( name like ? and c.anniversary = ? ) or (c.status = ?  and c.id &gt; ? )
+ * 
+ * </pre>
  */
 public interface Junction<T> extends Expression, ExpressionList<T> {
-	
+
 }
