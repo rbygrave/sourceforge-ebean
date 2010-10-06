0c8f09d362f9886035a9896185a2d8d233958dc1
imario <imario@391a5f12-5583-4198-a4bc-3c26488f1c65>
2010-10-06 05:55:28 +0000

    show ebean failing with subquery and inheritance

--- ebean/trunk/src/test/java/com/avaje/tests/query/TestSubQuery.java
+++ ebean/trunk/src/test/java/com/avaje/tests/query/TestSubQuery.java
@@ -1,15 +1,16 @@
 package com.avaje.tests.query;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import junit.framework.TestCase;
-
 import com.avaje.ebean.Ebean;
 import com.avaje.ebean.Query;
 import com.avaje.tests.model.basic.CKeyParent;
 import com.avaje.tests.model.basic.Order;
 import com.avaje.tests.model.basic.ResetBasicData;
+import com.avaje.tests.model.basic.Vehicle;
+import com.avaje.tests.model.basic.VehicleDriver;
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestSubQuery extends TestCase {
 
@@ -72,5 +73,133 @@ public class TestSubQuery extends TestCase {
 			fail("golden string not found");
 		}
 	}
-	
+
+	/**
+	 * show that ebean is not using the correct table name in the subquery (sq)
+	 */
+	public void testInheritance1()
+	{
+		ResetBasicData.reset();
+
+		Query<Vehicle> sq = Ebean.createQuery(Vehicle.class)
+			.select("id")
+			.setAutofetch(false)
+			.where()
+			.query();
+
+		Query<VehicleDriver> pq = Ebean.find(VehicleDriver.class)
+			.where().in("vehicle.id", sq)
+			.query();
+
+		pq.findList();
+
+		String sql = pq.getGeneratedSql();
+		System.err.println(sql);
+
+		String golden = "(t0.vehicle_id) in (select t0.id from t0.vehicle t0)";
+		if (sql.indexOf(golden) < 0)
+		{
+			System.out.println("failed sql:"+sql);
+			fail("golden string not found");
+		}
+
+	}
+
+	/**
+	 * show that ebean is adding the discriminator to the list of columns in the subquery
+	 */
+	public void testInheritance2()
+	{
+		ResetBasicData.reset();
+
+		Query<VehicleDriver> sq = Ebean.createQuery(VehicleDriver.class)
+			.select("vehicle")
+			.setAutofetch(false)
+			.where()
+			.query();
+
+		Query<Vehicle> pq = Ebean.find(Vehicle.class)
+			.where().in("id", sq)
+			.query();
+
+		pq.findList();
+
+		String sql = pq.getGeneratedSql();
+		System.err.println(sql);
+
+		// TODO: If, after bugfixing, the system still join against vehicle I do not know now, in our case, it is not necessary if not
+		// using it in the where clause
+		String golden = "(t0.id) in (select t0.vehicle_id  from vehicle_driver t0 left outer join vehicle t1 on t1.id = t0.vehicle_id )";
+		if (sql.indexOf(golden) < 0)
+		{
+			System.out.println("failed sql:"+sql);
+			fail("golden string not found");
+		}
+
+	}
+
+	/**
+	 * show that ebean is adding the discriminator to the list of columns in the subquery.
+	 * Second test to make sure that joining is still possible after bugfixing testInheritance2.
+	 */
+	public void testInheritance3()
+	{
+		ResetBasicData.reset();
+
+		Query<VehicleDriver> sq = Ebean.createQuery(VehicleDriver.class)
+			.select("vehicle")
+			.setAutofetch(false)
+			.where()
+			.eq("vehicle.licenseNumber", "abc")
+			.query();
+
+		Query<Vehicle> pq = Ebean.find(Vehicle.class)
+			.where().in("id", sq)
+			.query();
+
+		pq.findList();
+
+		String sql = pq.getGeneratedSql();
+		System.err.println(sql);
+
+		String golden = "(t0.id) in (select t0.vehicle_id  from vehicle_driver t0 left outer join vehicle t1 on t1.id = t0.vehicle_id   where t1.license_number = ? )";
+		if (sql.indexOf(golden) < 0)
+		{
+			System.out.println("failed sql:"+sql);
+			fail("golden string not found");
+		}
+	}
+
+	/**
+	 * show that ebean is using the wrong column name for the selected column in the subquery.
+	 * In contrast to testInheritance2+3 this test forces ebean to "drill down" to the key of the relation.
+	 */
+	public void testInheritance4()
+	{
+		ResetBasicData.reset();
+
+		Query<VehicleDriver> sq = Ebean.createQuery(VehicleDriver.class)
+			.select("vehicle.id")
+			.setAutofetch(false)
+			.where()
+			.query();
+
+		Query<Vehicle> pq = Ebean.find(Vehicle.class)
+			.where().in("id", sq)
+			.query();
+
+		pq.findList();
+
+		String sql = pq.getGeneratedSql();
+		System.err.println(sql);
+
+		String golden = "(t0.id) in (select t1.id  from vehicle_driver t0 left outer join vehicle t1 on t1.id = t0.vehicle_id)";
+		// OR without join
+		// String golden = "(t0.id) in (select t0.vehicle_id  from vehicle_driver t0)";
+		if (sql.indexOf(golden) < 0)
+		{
+			System.out.println("failed sql:"+sql);
+			fail("golden string not found");
+		}
+	}
 }
