d2d8fa26bc398d1ddc2b27b6ce6de7441ab387b0
Rob Bygrave <208973+rbygrave@users.noreply.github.com>
2010-10-06 08:03:29 +0000

    Change to extend ScalarTypeBaseVarchar

--- ebean/trunk/src/main/java/com/avaje/ebeaninternal/server/type/ScalarTypeClass.java
+++ ebean/trunk/src/main/java/com/avaje/ebeaninternal/server/type/ScalarTypeClass.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2006  Robin Bygrave
+ * Copyright (C) 2010  Authors
  * 
  * This file is part of Ebean.
  * 
@@ -19,167 +19,48 @@
  */
 package com.avaje.ebeaninternal.server.type;
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.sql.Types;
-import java.util.logging.Logger;
-
-import com.avaje.ebeaninternal.server.lucene.LLuceneTypes;
+import javax.persistence.PersistenceException;
 
 /**
- * ScalarType for Class<?>.
+ * ScalarType for Class that persists it to VARCHAR column.
+ * 
+ * @author emcgreal
+ * @author rbygrave
  */
-public class ScalarTypeClass extends ScalarTypeBase<Class<?>> {
+@SuppressWarnings("unchecked")
+public class ScalarTypeClass extends ScalarTypeBaseVarchar<Class> {
 	
-	/** The Constant logger. */
-	private static final Logger logger = Logger.getLogger(ScalarTypeClass.class.getName());
-	
-	
-	/**
-	 * Instantiates a new scalar type class.
-	 */
-	public ScalarTypeClass() {
-		super(null, false, Types.VARCHAR);
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.avaje.ebeaninternal.server.type.ScalarType#bind(com.avaje.ebeaninternal.server.type.DataBind, java.lang.Object)
-	 */
-	public void bind(DataBind b, Class<?> value) throws SQLException {
-		if (value == null){
-			b.setNull(Types.VARCHAR);
-		} else {
-			b.setString(value.getCanonicalName());
-		}
-	}
-
-	/* (non-Javadoc)
-	 * @see com.avaje.ebeaninternal.server.type.ScalarType#read(com.avaje.ebeaninternal.server.type.DataReader)
-	 */
-	public Class<?> read(DataReader dataReader) throws SQLException {
-		final String s = dataReader.getString();
-		
-		if (s != null){
-			return parse(s);
-		}
-		
-		return null;
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.avaje.ebeaninternal.server.type.ScalarType#toJdbcType(java.lang.Object)
-	 */
-	public Object toJdbcType(Object value) {
-		Class<?> c= (Class<?>) value;
-		return c != null ? c.getCanonicalName() : null;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.avaje.ebeaninternal.server.type.ScalarType#toBeanType(java.lang.Object)
-	 */
-	public Class<?> toBeanType(Object value) {
-		if (value != null){
-			return parse(value.toString());
-		}
-		
-		return null;
-	}
-
-    /* (non-Javadoc)
-     * @see com.avaje.ebeaninternal.server.type.ScalarType#formatValue(java.lang.Object)
-     */
-    public String formatValue(Class<?> t) {
-        return t != null ? t.getCanonicalName() : null;
-    }
-	
-	/* (non-Javadoc)
-	 * @see com.avaje.ebeaninternal.server.type.ScalarType#parse(java.lang.String)
-	 */
-	public Class<?> parse(String value) {
-		try {
-			return Class.forName(value.toString());
-		} catch (ClassNotFoundException e) {
-			// ~EMG should we throw an runtime exception here??
-			logger.fine(e.getMessage());
-		}
-		
-		return null;
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.avaje.ebeaninternal.server.type.ScalarType#isDateTimeCapable()
-	 */
-	public boolean isDateTimeCapable() {
-		return false;
-	}
-
-    /**
-     * To json string.
-     * 
-     * @param value the value
-     * 
-     * @return the string
-     */
-    public String toJsonString(Class<?> value) {
-        if(value == null) {
-            return "null";
-        } else {
-            return value.getCanonicalName();
-        }
+    public ScalarTypeClass() {
+        super(Class.class);
     }
     
-    /* (non-Javadoc)
-     * @see com.avaje.ebeaninternal.server.type.ScalarType#getLuceneType()
-     */
-    public int getLuceneType() {
-        return LLuceneTypes.STRING;
+    @Override
+    public int getLength() {
+        return 255;
     }
 
-    /* (non-Javadoc)
-     * @see com.avaje.ebeaninternal.server.type.ScalarType#luceneFromIndexValue(java.lang.Object)
-     */
-    public Object luceneFromIndexValue(Object value) {
-        return value;
-    }
-
-    /* (non-Javadoc)
-     * @see com.avaje.ebeaninternal.server.type.ScalarType#luceneToIndexValue(java.lang.Object)
-     */
-    public Object luceneToIndexValue(Object value) {
-        return value;
+    @Override
+    public Class<?> convertFromDbString(String dbValue) {
+        return parse(dbValue);
     }
     
-    /* (non-Javadoc)
-     * @see com.avaje.ebeaninternal.server.type.ScalarType#readData(java.io.DataInput)
-     */
-    public Object readData(DataInput dataInput) throws IOException {
-        if (!dataInput.readBoolean()) {
-            return null;
-        } else {
-            return dataInput.readUTF();
-        }
+    @Override
+    public String convertToDbString(Class beanValue) {
+        return beanValue.getCanonicalName();
     }
 
-    /* (non-Javadoc)
-     * @see com.avaje.ebeaninternal.server.type.ScalarType#writeData(java.io.DataOutput, java.lang.Object)
-     */
-    public void writeData(DataOutput dataOutput, Object v) throws IOException {
-        
-        Class<?> value = (Class<?>)v;
-        if (value == null){
-            dataOutput.writeBoolean(false);
-        } else {
-            dataOutput.writeBoolean(true);
-            dataOutput.writeUTF(value.getCanonicalName());            
-        }
+    public String formatValue(Class v) {
+        return v.getCanonicalName();
     }
 
-	/* (non-Javadoc)
-	 * @see com.avaje.ebeaninternal.server.type.ScalarType#parseDateTime(long)
-	 */
-	public Class<?> parseDateTime(long dateTime) {
-		return null;
-	}
+    public Class<?> parse(String value) {
+        try {
+            return Class.forName(value);
+        } catch (Exception e) {
+            String msg = "Unable to find Class "+value;
+            throw new PersistenceException(msg, e);
+        }
+    }
+	
+	
 }
