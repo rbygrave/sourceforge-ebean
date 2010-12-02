1616cbae008a2ee58d6d51c82ecd12594af5b0c2
Rob Bygrave <208973+rbygrave@users.noreply.github.com>
2010-12-02 08:51:35 +0000

    Update Ebean version String

--- ebean/trunk/src/main/java/com/avaje/ebean/Ebean.java
+++ ebean/trunk/src/main/java/com/avaje/ebean/Ebean.java
@@ -140,7 +140,7 @@ public final class Ebean {
     /**
      * The version and date of build.
      */
-    private static final String EBVERSION = "2.7.0-SNAPSHOT";
+    private static final String EBVERSION = "2.7.2";
 
     static {
         String version = System.getProperty("java.version");
