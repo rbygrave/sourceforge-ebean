406bcdfbdfdb6b67c5afebee778a894920214e66
rob107 <rob107@391a5f12-5583-4198-a4bc-3c26488f1c65>
2011-06-13 20:12:39 +0000

    Test and fix for BUG 368 : Should never be called on com.company.domain.AbstractBar.foo

--- ebean/trunk/src/main/java/com/avaje/ebeaninternal/server/deploy/ReflectSetter.java
+++ ebean/trunk/src/main/java/com/avaje/ebeaninternal/server/deploy/ReflectSetter.java
@@ -19,6 +19,8 @@
  */
 package com.avaje.ebeaninternal.server.deploy;
 
+import java.lang.reflect.Method;
+
 import com.avaje.ebeaninternal.server.deploy.meta.DeployBeanProperty;
 import com.avaje.ebeaninternal.server.reflect.BeanReflectSetter;
 
@@ -36,24 +38,35 @@ public class ReflectSetter {
 	 * Creates place holder objects that should never be called.
 	 */
 	public static BeanReflectSetter create(DeployBeanProperty prop) {
-		return new NeverCalled(prop.getFullBeanName());
+		
+		String fullName = prop.getFullBeanName();
+		Method writeMethod = prop.getWriteMethod();
+		return new RefCalled(fullName, writeMethod);
 	}
 	
-	public static class NeverCalled implements BeanReflectSetter {
-
-		private final String property;
+	static class RefCalled implements BeanReflectSetter {
 		
-		public NeverCalled(String property){
-			this.property = property;
+		final String fullName;
+		final Method writeMethod;
+		
+		RefCalled(String fullName, Method writeMethod) {
+			this.fullName = fullName;
+			this.writeMethod = writeMethod;
 		}
-		
 		public void set(Object bean, Object value) {
-			throw new RuntimeException("Should never be called on "+property);
+			Object[] a = new Object[1];
+			a[0] = value;
+			try {
+	            writeMethod.invoke(bean, a);
+            } catch (Exception e) {
+            	String beanType = bean == null ? "null" : bean.getClass().toString();
+            	String msg = "Error setting value on "+fullName+" value["+value+"] on type["+beanType+"]";
+	            throw new RuntimeException(msg, e);
+            } 
 		}
-
 		public void setIntercept(Object bean, Object value) {
-			throw new RuntimeException("Should never be called on "+property);
+			String msg = "Not expecting setIntercept to be called. Refer Bug 368";
+			throw new RuntimeException(msg);
 		}
-				
 	}
 }
