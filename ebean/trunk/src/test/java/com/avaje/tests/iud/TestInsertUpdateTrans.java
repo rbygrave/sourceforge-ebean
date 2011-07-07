c732f1a46d52e38fc708a5890fd761b64db4f269
rob107 <rob107@391a5f12-5583-4198-a4bc-3c26488f1c65>
2011-07-07 08:12:52 +0000

    Fix for BUG 370 : Ability to set validateOnSave in ebean.properties

--- ebean/trunk/src/test/java/com/avaje/tests/iud/TestInsertUpdateTrans.java
+++ ebean/trunk/src/test/java/com/avaje/tests/iud/TestInsertUpdateTrans.java
@@ -1,7 +1,5 @@
 package com.avaje.tests.iud;
 
-import java.sql.Timestamp;
-
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
@@ -20,14 +18,9 @@ public class TestInsertUpdateTrans extends TestCase {
 			e0.setDescription("something");
 			Ebean.save(e0);
 
-			// // only use the below test when not using enhancement
-			// boolean entity = (e0 instanceof EntityBean);
-			// Assert.assertTrue(!entity);
-
 			Assert.assertNotNull(e0.getId());
 			Assert.assertNotNull(e0.getLastUpdate());
-			Timestamp lastUpdate0 = e0.getLastUpdate();
-			
+			Assert.assertEquals("onInsert", e0.getName());			
 			
 			e0.setName("onUpdate");
 			e0.setDescription("differentFromInsert");
@@ -36,14 +29,10 @@ public class TestInsertUpdateTrans extends TestCase {
 			
 			EBasicVer e1 = Ebean.find(EBasicVer.class, e0.getId());
 			
-			Timestamp lastUpdate1 = e1.getLastUpdate();
-			
 			// we should fetch back the updated data (not inserted)
 			Assert.assertEquals(e0.getId(), e1.getId());
 			Assert.assertEquals("onUpdate", e1.getName());
 			Assert.assertEquals("differentFromInsert", e1.getDescription());
-			long diff = lastUpdate1.getTime() - lastUpdate0.getTime();
-			Assert.assertTrue(diff > 0);
 			
 			
 			Ebean.commitTransaction();
