d8c9eb4aca094c143c88cd73dd38eb48df6da20a
rob107 <rob107@391a5f12-5583-4198-a4bc-3c26488f1c65>
2012-01-08 22:52:45 +0000

    Serialisable Product ... change ROrderPK equals

--- ebean/trunk/src/test/java/com/avaje/tests/model/basic/Product.java
+++ ebean/trunk/src/test/java/com/avaje/tests/model/basic/Product.java
@@ -1,5 +1,6 @@
 package com.avaje.tests.model.basic;
 
+import java.io.Serializable;
 import java.sql.Timestamp;
 
 import javax.persistence.Entity;
@@ -17,7 +18,9 @@ import com.avaje.ebean.validation.Length;
 @CacheStrategy(warmingQuery="order by name")
 @Entity
 @Table(name="o_product")
-public class Product {
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     Integer id;
